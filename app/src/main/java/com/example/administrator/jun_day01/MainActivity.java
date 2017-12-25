b178db6360836116b146846b2a4b0e523cee52ab
Administrator <1210083440@qq.com>
2017-12-25 20:04:25 +0800

    add img

--- app/src/main/java/com/example/administrator/jun_day01/MainActivity.java
+++ app/src/main/java/com/example/administrator/jun_day01/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.administrator.jun_day01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTv;
+    private ImageView mIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mTv = (TextView) findViewById(R.id.tv);
+        mIv = (ImageView) findViewById(R.id.iv);
     }
 }
