961442c4d1189c26291b61e7f3e45b1b8bb0a3be
Administrator <1210083440@qq.com>
2017-12-25 20:12:38 +0800

    add img

--- app/src/main/java/com/example/administrator/jun_day01/MainActivity.java
+++ app/src/main/java/com/example/administrator/jun_day01/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.administrator.jun_day01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn){
+
+        }
+    }
+
+    private void initView() {
+        mBtn = (Button) findViewById(R.id.btn);
+        mBtn.setOnClickListener(this);
     }
 }
